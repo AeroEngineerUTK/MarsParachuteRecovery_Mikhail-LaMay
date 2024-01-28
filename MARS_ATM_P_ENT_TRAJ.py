bfadde72e1e51b63b6b211cff2f88500f2cc5d8c
UTKAeroEngineer <mikhailmaxwelllamay@gmail.com>
2024-01-27 19:36:01 -0500

    MARS_ATM_P_ENT_TRAJ.py: Recreated Script, Cleaner MARS_ATM_P_ENT_TRAJ.txt: Cleaned Up Indexing In NotePad++

--- MARS_ATM_P_ENT_TRAJ.py
+++ MARS_ATM_P_ENT_TRAJ.py
@@ -1,77 +1,150 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 17 12:21:23 2020
 
-@author: Mikhail LaMay
+# Author: Mikhail LaMay
+
+# --------------------------------------------------------------------
+# MARS_ATM_P_ENT_TRAJ contains scripts to read MARS_ATM_P_ENT_TRAJ.txt
+# and perform various calculations for MARS probe Re-Entry. It is used
+# seperatley from Spacecraft_Reentry_Trajectory_Code.pynb and differs
+# with it being a sandbox for calculations.
+# --------------------------------------------------------------------
+
+
+
+
+#pyspread is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyspread is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyspread.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
+
+
+# Enter Below Functions Used Within This Python Script
 """
-# Modules
+
+**Provides**
+
+* :func: ' ': 
+
+"""
+
+# Define Libraries
+import numpy as np
+from numpy import diff
 import pandas as pd
 import matplotlib.pyplot as plt
-from numpy import diff
-import numpy as np
-import math
 
-# Open's MARS_ATM_P_ENT_TRAJ_v2.txt & RR Variables
-#with open('MARS_ATM_P_ENT_TRAJ.txt', 'r') as reader:
-    # Read & print the entire file
-    #print(reader.read())
-df = pd.read_csv('MARS_ATM_P_ENT_TRAJ_v2.txt', sep='\t', lineterminator='\n')
-#print(df)
+# Define Debug Variables
+printData_Active = False
+printOutput_Active = False
 
-# Variables
-time = df['t(s)']                                   # sec
-alt = df['Alt(m)']                                  # m
-vel = df['Vel(m/s)']                                # m/s
-gamma = df['Gamma']                                 # degree    
-den = df['Den(kg/m^3)\r']                           # kg/m^3
+# --------------------------------------------------------------------
+# Accessing Mars Atmospheric Model
+""" Atmopsheric Model Variables:
+    
+    t(s)
+    Alt(m)
+    Vel(m/s)
+    Gamma
+    Den(kg/m^3)
 
-G = 3.711                                           # m/s^2
-R_Mars = 3389.5e3                                   # m
-Mass_Pr = 300                                       # kg
-Area_Pr = 3.14                                      # m^2
+"""
+# --------------------------------------------------------------------
 
+# Open MARS_ATM_P_ENT_TRAJ.txt and Print To Terminal
+if printData_Active:
+    with open('MARS_ATM_P_ENT_TRAJ.txt', 'r') as reader:
+        print(reader.read())
 
-# Re - Entry
+# Read MARS_ATM_P_ENT_TRAJ.txt into a DataFrame
+dataFrame = pd.read_csv('MARS_ATM_P_ENT_TRAJ.txt', sep='\t', lineterminator='\n')
+#print(dataFrame)
 
-# Dynamic Pressue
-q = (den * vel**2) / 2                              # Pa
+# Define Variables From MARS_ATM_P_ENT_TRAJ.txt
+time = dataFrame['t(s)']    # time(sec)
+alt = dataFrame['Alt(m)']   # altitude(m)
+vel = dataFrame['Vel(m/s)'] # velocity(m/s)
+gamma = dataFrame['Gamma']  # gamma(degree)    
+den = dataFrame['Den(kg/m^3)\r']    # density(kg/m^3)
 
-# Acceleration
-x = df['t(s)']                                      # s
-y = df['Vel(m/s)']                                  # m/s
-#accel = np.diff(y)/np.diff(x)                      # m/s^2
-accel = -1*(y.diff())
-#accel_corrected = np.insert(accel,0,[0])
-#print(accel_corrected)
+# Define Constants
+G = 3.711   # gravity(m/s^2)
+R_Mars = 3389.5e3   # Mar's Radius(m)
+Mass_Pr = 300   # ProbeMass(kg)
+Area_Pr = 3.14  # ProbeArea(m^2)
 
-# Force
-F = (Mass_Pr * accel)                                 # N @ alt
+# --------------------------------------------------------------------
+# Re-entry Calculations
+# --------------------------------------------------------------------
 
-# Weight
+# Calculate Dynamic Pressue
+q = (den*(vel**2))/2  # Pa
+
+# Calculate Acceleration
+def acceleration(velocity):
+
+    acceleration = -1*(velocity.diff())    # m/s^2
+
+    return acceleration
+accel_Pr = acceleration(vel)    # m/s^2
+
+# Calculate Force
+def Force(Mass_Probe,acceleration):
+
+    Force = Mass_Probe * acceleration    # N @ alt
+    
+    return Force
+F = Force(Mass_Pr, accel_Pr)    # N @ alt
+
+# Calculate Gravity
 G_alt = G * (R_Mars**2)/((R_Mars + alt)**2)         # m/s^2 @ alt
+
+# Calculate Weight of Probe
 W_alt = G_alt * Mass_Pr                             # N @ alt
 
-# Drag Coefficient
+# Calculate Drag Coefficient
 gamma_rad = np.deg2rad(gamma)
 C_1 = np.sin(gamma_rad)
 C_2 = np.cos(2*gamma_rad)
+C_3 = 2*W_alt*C_1
+C_4 = np.sqrt(2)
+C_5 = np.sqrt(np.absolute((2*(F**2) - (W_alt**2)*(C_2) - (W_alt**2))))
+C_6 = den * vel**2 * Area_Pr
+C_D = (-1* ((C_3 - (C_4 * C_5)) / C_6)) #C_D @ Alt
 
+if printOutput_Active:
+    print("----Dynamic Pressure----")
+    print(q)
+    print("----Re-Entry Probe Acceleration----")
+    print(accel_Pr)
+    print("----Re-Entry Probe Force----")
+    print(F)
+    print("----Re-Entry Mars Gravity----")
+    print(G_alt)
+    print("----Re-Entry Weight of Probe---")
+    print(W_alt)
+    print("----Drag Coefficient----")
+    print(C_D)
 
-A = 2*W_alt*C_1
-B = np.sqrt(2)
-C = np.sqrt(np.absolute((2*(F**2) - (W_alt**2)*(C_2) - (W_alt**2))))
-D = den * vel**2 * Area_Pr
+# --------------------------------------------------------------------
+# Plot Data
+# --------------------------------------------------------------------
 
-C_D = -1* ((A - (B * C)) / D)
-print(C_D)
-#C_D = (2*W_alt * C_1 - ((np.sqrt(2)*np.sqrt((2*F**2) - ((W_alt**2) * C_2) - W_alt))) / (den * vel**2 * Area_Pr)
+# Clean Up Time/Acceleration Data From NAN Data
+updatedTime = time.drop(index=0)
+updatedAccel = accel_Pr.drop(index=0)
 
-#gamma_rad = np.deg2rad(gamma)
-
-
-time_accel = plt.plot(time, accel)
-plt.show(time_accel)
-
-# Re-Entry Trajectory Plot
-#df.plot(kind='scatter', x='t(s)', y='Alt(m)')
-#plt.show()
+# Plot Acceleration v. Time
+plt.figure()
+plt.plot(updatedTime,updatedAccel, 'g')
+plt.xlabel('Time, s', fontsize=10)
+plt.ylabel('Acceleration, m/s^2', fontsize=10)
+plt.show()
